a3156545489c386b2e47525b47ac84e8e5ceb991
bole25 <mbojan13@gmail.com>
2019-11-28 16:44:50 +0100

    deleting requests and sending explanation mail

--- src/main/java/com/clinicalcenter/com/clinicalsys/controller/ReuqestsController.java
+++ src/main/java/com/clinicalcenter/com/clinicalsys/controller/ReuqestsController.java
@@ -2,11 +2,16 @@ package com.clinicalcenter.com.clinicalsys.controller;
 
 import com.clinicalcenter.com.clinicalsys.model.User;
 import com.clinicalcenter.com.clinicalsys.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.mail.javamail.JavaMailSender;
+
 
 import java.util.Set;
 
@@ -15,6 +20,11 @@ import java.util.Set;
 public class ReuqestsController {
 
     private final UserRepository userRepository;
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private Environment env;
 
     public ReuqestsController(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -28,4 +38,30 @@ public class ReuqestsController {
 
         return new ResponseEntity<>(retValue, HttpStatus.OK);
     }
+
+    @Async
+    @DeleteMapping("/deleterequest/{email}/{content}")
+    public ResponseEntity<String> removeRequest(@PathVariable("email") String email, @PathVariable("content") String content) throws MailException, InterruptedException{
+        User us = userRepository.findByEmail(email);
+        if(us == null){
+            System.out.println("User with email: " + email + " dose not exist!");
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+        else {
+            System.out.println("Deleting..." + email);
+            System.out.println("Content: " + content);
+
+            System.out.println("Slanje emaila...");
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(email);
+            mail.setFrom("spring.mail.username");
+            mail.setSubject("Rejection");
+            mail.setText("Explanation for reqistration rejection: \n" + content);
+            this.mailSender.send(mail);
+            System.out.println("Email sent..");
+            userRepository.deleteByEmail(email);
+            return new ResponseEntity<>("", HttpStatus.OK);
+        }
+    }
+
 }
--- src/main/java/com/clinicalcenter/com/clinicalsys/repository/UserRepository.java
+++ src/main/java/com/clinicalcenter/com/clinicalsys/repository/UserRepository.java
@@ -6,12 +6,18 @@ package com.clinicalcenter.com.clinicalsys.repository;
         import org.springframework.data.jpa.repository.Query;
         import org.springframework.stereotype.Repository;
 
+        import javax.transaction.Transactional;
         import java.util.Set;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+
+
 
     @Query("SELECT u FROM User u WHERE u.active = false ")
     Set<User> findRequests();
