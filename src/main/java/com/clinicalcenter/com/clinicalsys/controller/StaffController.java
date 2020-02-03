63d01e8f6d3a509dce7867bb22471735abddbb76
Milica Medic <milica.medic@gmail.com>
2020-02-03 18:01:01 +0100

    Vacation request completed

--- src/main/java/com/clinicalcenter/com/clinicalsys/controller/StaffController.java
+++ src/main/java/com/clinicalcenter/com/clinicalsys/controller/StaffController.java
@@ -1,22 +1,29 @@
 package com.clinicalcenter.com.clinicalsys.controller;
 
+import com.clinicalcenter.com.clinicalsys.model.Staff;
 import com.clinicalcenter.com.clinicalsys.model.User;
+import com.clinicalcenter.com.clinicalsys.model.VacationRequest;
+import com.clinicalcenter.com.clinicalsys.model.enumeration.RoleEnum;
+import com.clinicalcenter.com.clinicalsys.repository.StaffRepository;
 import com.clinicalcenter.com.clinicalsys.repository.UserRepository;
+import com.clinicalcenter.com.clinicalsys.util.Authorized;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 public class StaffController {
     private final UserRepository userRepository;
-
-    public StaffController(UserRepository userRepository) {
+    @Autowired
+    private StaffRepository staffRepository;
+    public StaffController(UserRepository userRepository, StaffRepository staffRepository) {
         this.userRepository = userRepository;
+        this.staffRepository = staffRepository;
     }
 
 
@@ -28,75 +35,34 @@ public class StaffController {
             return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
         }*/
         System.out.println("StaffController");
-        Set<User> retValue = new Set<User>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<User> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(User user) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends User> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-        };
+        Set<User> retValue = new HashSet<User>();
         retValue = userRepository.findStaff();
         return new ResponseEntity<>(retValue, HttpStatus.OK);
     }
 
+    /*@GetMapping("/getObligations/{email}")
+    public ResponseEntity<Staff_Obligations> getObligations(@PathVariable("email") String email){
+
+        Staff staff=staffRepository.findByEmail(email);
+        if(staff==null){
+            return new ResponseEntity<>(null,HttpStatus.NOT_ACCEPTABLE);
+        }
+        appointmentsstaff.getAppointments()
+    }*/
+
+    @PostMapping("/requestVacation/{email}")
+    public ResponseEntity<String> requestVacation(@RequestBody VacationRequest vr,
+                                                  @PathVariable("email") String email){
+        if(!Authorized.isAuthorised(email)){
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        }
+        Staff staff=staffRepository.findByEmail(email);
+        if(staff==null){
+            return new ResponseEntity<>(null,HttpStatus.NOT_ACCEPTABLE);
+        }
+        staff.getVacReq().add(vr);
+        staffRepository.save(staff);
+        return new ResponseEntity<>(null,HttpStatus.OK);
+    }
 
 }
