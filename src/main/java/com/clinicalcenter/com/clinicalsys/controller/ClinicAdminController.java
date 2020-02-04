79f1d0d219d8d2b0e9baa74b7f17658ea09b1fda
Milica Medic <milica.medic@gmail.com>
2020-02-04 22:53:33 +0100

    Few bugs fixed

--- src/main/java/com/clinicalcenter/com/clinicalsys/controller/ClinicAdminController.java
+++ src/main/java/com/clinicalcenter/com/clinicalsys/controller/ClinicAdminController.java
@@ -1,6 +1,7 @@
 package com.clinicalcenter.com.clinicalsys.controller;
 
 import com.clinicalcenter.com.clinicalsys.model.*;
+import com.clinicalcenter.com.clinicalsys.model.DTO.Doctor_FreeTimes;
 import com.clinicalcenter.com.clinicalsys.model.enumeration.AppStateEnum;
 import com.clinicalcenter.com.clinicalsys.model.enumeration.RoleEnum;
 import com.clinicalcenter.com.clinicalsys.model.enumeration.RoomType;
@@ -14,10 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @RestController
 @RequestMapping("/admin")
@@ -25,7 +23,6 @@ public class ClinicAdminController {
 
     @Autowired
     private NotifyUserSrvice notifyUserSrvice;
-
     @Autowired
     private ClinicAdminRepository clinicAdminRepository;
     @Autowired
@@ -87,6 +84,9 @@ public class ClinicAdminController {
     @GetMapping("/assignRoomToAppointment/{roomId}/{appId}")
     public ResponseEntity<String> assignRoomToAppointment(@PathVariable("roomId") String roomId,
                                                           @PathVariable("appId") String appId){
+        if(!Authorized.isAuthorised(RoleEnum.CLINIC_ADMIN)){
+            return new ResponseEntity<>(null,HttpStatus.FORBIDDEN);
+        }
         Long app_id,room_id;
         try{
             app_id = Long.parseLong(appId);
@@ -114,22 +114,94 @@ public class ClinicAdminController {
 
     @PostMapping("/rejectAppointment/{appId}")
     public ResponseEntity<String> rejectAppointment(@PathVariable("appId") String appId){
+        if(!Authorized.isAuthorised(RoleEnum.CLINIC_ADMIN)){
+            return new ResponseEntity<>(null,HttpStatus.FORBIDDEN);
+        }
         Long app_id;
         try{
             app_id = Long.parseLong(appId);
         }catch (Exception e){
             return new ResponseEntity<>(null,HttpStatus.NOT_ACCEPTABLE);
         }
-        Appointment app=appointmentRepository.findByIdMy(app_id);
-        Set<ClinicAdmin> clinicAdmins = clinicAdminRepository.getByDoctorEmail(app.getDoctor().getEmail());
-        for (ClinicAdmin admin:clinicAdmins){
-            admin.getAppointments_to_process().remove(app);
-            admin = clinicAdminRepository.save(admin);
-        }
-        app.setAppState(AppStateEnum.REJECTED);
-        appointmentRepository.save(app);
+        new Thread(() -> {
+            Appointment app=appointmentRepository.findByIdMy(app_id);
+            Set<ClinicAdmin> clinicAdmins = clinicAdminRepository.getByDoctorEmail(app.getDoctor().getEmail());
+            for (ClinicAdmin admin:clinicAdmins){
+                admin.getAppointments_to_process().remove(app);
+                admin = clinicAdminRepository.save(admin);
+            }
+            app.setAppState(AppStateEnum.REJECTED);
+            appointmentRepository.save(app);
+        }).start();
         return new ResponseEntity<>(null,HttpStatus.OK);
     }
+
+
+    //SECTION FOR PREDEFINED APPOINTMENTS
+    /*Returns Doctors that are specialized in given AppointmentType, work for that clinic and are free at defined time*/
+    @PostMapping("/getAvailablDoctors/{date}/{clinicName}")
+    public ResponseEntity<Set<Doctor>> getFreeSpecializedDoctors(@RequestBody AppointmentType appType,
+                                                                           @PathVariable("date") String date_string,
+                                                                           @PathVariable("clinicName") String clinic_name) {
+        if(!Authorized.isAuthorised(RoleEnum.CLINIC_ADMIN)){
+            return new ResponseEntity<>(null,HttpStatus.FORBIDDEN);
+        }
+        String pattern = "yyyy-MM-dd HH:mm:ss";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date date;
+        try {
+            date = simpleDateFormat.parse(date_string);
+        } catch (ParseException e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_ACCEPTABLE);
+        }
+        Clinic clinic = clinicRespository.findByClinicName(clinic_name);
+        Set<Doctor> doctors_temp= new HashSet<Doctor>(doctorRepository.allWithSpecializationInClinic(appType.getId(),clinic.getId()));
+        Set<Doctor> retVal=new HashSet<>();
+        Calendar start = Calendar.getInstance();
+        start.setTime(date);
+        start.set(Calendar.SECOND,0);
+        start.set(Calendar.MILLISECOND,0);
+        Calendar end = (Calendar) start.clone();
+        end.add(Calendar.MINUTE, 30);
+        for(Doctor doctor : doctors_temp){
+            if(doctor.checkIfAppFree(start,end))
+                retVal.add(doctor);
+        }
+        return new ResponseEntity<>(retVal,HttpStatus.OK);
+    }
+
+    @GetMapping("getClinicAppointmentTypes/{email}")
+    public ResponseEntity<Set<AppointmentType>> getClinicAppointmentTypes(@PathVariable("email") String email){
+        if(!Authorized.isAuthorised(RoleEnum.CLINIC_ADMIN)){
+            return new ResponseEntity<>(null,HttpStatus.FORBIDDEN);
+        }
+        ClinicAdmin clinicAdmin = clinicAdminRepository.findByEmail(email);
+        Set<AppointmentType_Price_Discount> atpd = clinicAdmin.getClinic().getAppointmentTypePriceDiscounts();
+        Set<AppointmentType> retVal = new HashSet<>();
+        for (AppointmentType_Price_Discount i : atpd){
+            retVal.add(i.getAppointmentType());
+        }
+        return new ResponseEntity<>(retVal,HttpStatus.OK);
+    }
+
+    @PostMapping("/addPredefinedAppointment/{email}")
+    public ResponseEntity<String> predefinedAppointment(@RequestBody Appointment app,
+                                                        @PathVariable("email") String email){
+        if(!Authorized.isAuthorised(email)){
+            return new ResponseEntity<>(null,HttpStatus.FORBIDDEN);
+        }
+        ClinicAdmin clinicAdmin = clinicAdminRepository.findByEmail(email);
+        app.setAppState(AppStateEnum.APPROVED);
+        Calendar end = Calendar.getInstance();
+        end.setTime(app.getStartTime());
+        end.add(Calendar.MINUTE,30);
+        app.setEndTime(end.getTime());
+        app = appointmentRepository.save(app);
+        clinicAdmin.getClinic().getPredefinedAppointments().add(app);
+        return new ResponseEntity<>(null,HttpStatus.OK);
+    }
+
+
 
     @GetMapping("/getVacationRequests/{email}")
     public ResponseEntity<Set<VacationRequest>> getVacationRequests(@PathVariable("email") String email){
